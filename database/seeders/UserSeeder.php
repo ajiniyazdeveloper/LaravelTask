86144c01e730050fd86f615cef69adb59c1502c4
Ajiniyaz Umirbaev <ajiniyazumirbaev@gmail.com>
2023-10-26 09:54:46 +0500

    User and Role tables created and manager seeding

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+
+            'name' => 'Manager',
+            'role_id' => 1,
+            'email' => 'manage@ajiniyaz.dev',
+            'password' => Hash::make('secret')
+        ]);
+    }
+}
